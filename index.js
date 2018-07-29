22beb2a42e5682a3cee1466ec7f2692c8aeb56f6
nfactorial <nfactorial@btopenworld.com>
2018-07-30 00:26:54 +0100

    Initial commit.

---
+++ index.js
@@ -0,0 +1,11 @@
+import BaseBuffer from './lib/buffer';
+import Renderer from './lib/renderer';
+import QuadIndexBuffer from './lib/buffer/quad_index_buffer';
+import {Extension} from "./lib/extensions";
+
+export {
+    BaseBuffer,
+    QuadIndexBuffer,
+    Renderer,
+    Extension
+};
