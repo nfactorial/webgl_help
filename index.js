0399c66b172aa2af69c8c6cba5e27491d8f2069a
nfactorial <nfactorial@btopenworld.com>
2018-07-30 01:44:46 +0100

    Minor updates and API adjustments.

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+import Shader from './lib/shader';
+import Program from './lib/program';
 import BaseBuffer from './lib/buffer';
 import Renderer from './lib/renderer';
 import QuadIndexBuffer from './lib/buffer/quad_index_buffer';
 import {Extension} from "./lib/extensions";
 
 export {
-    BaseBuffer,
-    QuadIndexBuffer,
+    Shader,
+    Program,
     Renderer,
-    Extension
+    Extension,
+    BaseBuffer,
+    QuadIndexBuffer
 };
