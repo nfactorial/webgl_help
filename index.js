6ef38694de6590082510c0decd57bb79500e7b88
nfactorial <nfactorial@btopenworld.com>
2018-09-01 15:43:37 +0100

    Added array_buffer and element_array_buffer objects.

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ import Shader from './lib/shader';
 import Program from './lib/program';
 import BaseBuffer from './lib/buffer';
 import Renderer from './lib/renderer';
-import QuadIndexBuffer from './lib/buffer/quad_index_buffer';
 import Extension from './lib/extensions';
+import ArrayBuffer from './lib/buffer/array_buffer';
+import QuadIndexBuffer from './lib/buffer/quad_index_buffer';
+import ElementArrayBuffer from './lib/buffer/element_array_buffer';
 import { PrimitiveType, CullMode, DepthCompare } from './lib/primitives';
 
 export {
@@ -12,6 +14,8 @@ export {
     Renderer,
     Extension,
     BaseBuffer,
+    ArrayBuffer,
+    ElementArrayBuffer,
     QuadIndexBuffer,
     PrimitiveType,
     DepthCompare,
